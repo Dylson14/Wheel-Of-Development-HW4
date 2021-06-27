857218436a0c41dfe5b5b85762a1d84cdedee07b
dylson <dylsonuni@gmail.com>
2021-06-27 23:13:30 +0100

    creating quizstartpage function in HTML & Javasct

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -14,9 +14,22 @@ THEN I can save my initials and my score */
 /* Creating a starEl variable that is fetching the startButton id */
 var startEl = document.querySelector("#startButton");
 var timerEl = document.querySelector("#countdown");
+var hideEl = document.querySelector("#questionsPage");
+var showEl = document.querySelector("#quizStartPage");
+
+hideEl.style.display = "none";
+
+function showQuestions() {
+console.log("showQuestions function has activated!");
+hideEl
+
+
+console.log("showQuestions function has ended.");
+}
 
 function startTimer() {
-  var timeLeft = 10000;
+    console.log("startTimer function has activated!");
+  var timeLeft = 1000;
 
   var timeInterval = setInterval(function () {
     if (timeLeft > 1) {
@@ -30,13 +43,18 @@ function startTimer() {
       clearInterval(timeInterval);
     }
   }, 1000);
+  console.log("startTimer function has ended");
 }
 
 function startGame() {
-  window.alert("Alert test to see if button works.");
-  /* The previous page will then be removed so that a new page will appear with questions. */
+  console.log("startGame function has activated!");
+  
 
+  console.log("startGame function has ended.");
+  /* The previous page will then be removed so that a new page will appear with questions. */
+  showQuestions();
   startTimer();
+  
 }
 
 startEl.addEventListener("click", startGame);
